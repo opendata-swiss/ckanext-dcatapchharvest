4be2c7d67533fb1f93f54360f94924fe0eeaefd6
kovalch <nataliia.kovalchuk90@gmail.com>
2021-12-07 10:57:08 +0100

    add comments from Rae to the uri test function

--- ckanext/dcatapchharvest/tests/test_swiss_schemaorg_profile_serialize.py
+++ ckanext/dcatapchharvest/tests/test_swiss_schemaorg_profile_serialize.py
@@ -191,65 +191,32 @@ class TestSchemaOrgProfileSerializeDataset(BaseSerializeTest):
             for value in values:
                 assert self._triple(g, dataset_ref, item[1], item[2](value))
 
-    # new test
     def test_graph_from_dataset_uri(self):
+        """"Tests that datasets with a uri from the test system
+        have that uri changed to reference the prod system when they are output as a graph"""
 
         dataset = {
             'id': '4b6fe9ca-dc77-4cec-92a4-55c6624a5bd6',
             'name': 'test-dataset',
             'title': 'Test DCAT dataset',
             'uri': 'https://test.example.com/dataset/foo',
-            #'url': 'http://example.com/ds1',
             'version': '1.0b',
             'metadata_created': '2015-06-26T15:21:09.034694',
-            'metadata_modified': '2015-06-26T15:21:09.075774'
+            'metadata_modified': '2015-06-26T15:21:09.075774',
+            'resources': [
+                {'uri': 'https://test.example.com/dataset/foo', 'id': '5f2be71f-636c-4d3f-aac1-50830b97f853'},
+            ]
         }
-        resources = {
-            "package_id": "bb14c897-eca0-47d0-a599-ae2e6faf839d",
-            "id": "bb957832-dad6-49d0-8096-4b245be196db",
-            "uri": "https://test.example.com/dataset/foo",
-            "revision_id": "be9bfa8a-6b2c-473c-8cc0-bb4b7391d500",
-            "identifier": "1c8d829b-0ce2-4d2d-ab54-f952d0f3818c",
-        }
+
         s = RDFSerializer(profiles=['swiss_schemaorg'])
         g = s.g
-        # Change dataset uri that includes a test url
-        dataset['uri'] = dh.dataset_uri(dataset)
         dataset_ref = s.graph_from_dataset(dataset)
 
-        # Change resource uri that includes a test url
-        resources['uri'] = dh.resource_uri(resources)
-        distribution = URIRef(resources['uri'])
+        # Change dataset uri that includes a test url
+        dataset_uri = dh.dataset_uri(dataset, dataset_ref)
+        dataset_ref_changed = URIRef(dataset_uri)
 
-        log.info("Checking resource_uri '%r'" % resources["uri"])
-        log.info("Checking distribution '%r'" % distribution)
-        g.add((dataset_ref, SCHEMA.distribution, distribution))
-        g.add((distribution, RDF.type, SCHEMA.Distribution))
-
-        #log.info(" 3 Checking dataset_ref dict '%r'" %= URIRef(dh.resource_uri(dataset.get('resources', []))))
-        # New Resource for datsets with changed uri
-        #dataset_ref_changed = URIRef(dataset_uri)
-
-        #for resource_dict in dataset.get('resources', []):
-        log.info("Checking resource_dict '%r'1: " % resources)
-
-        log.info("! Checking dataset_ref '%r'" % dataset_ref)
-
-        log.info("Checking dataset_ref '%r'" % dataset_ref)
-        log.info("Checking g '%r'" % g)
-        log.info("Checking RDF.type '%r'" % RDF.type)
-        log.info("Checking RDF '%r'" % RDF)
-        log.info("Checking SCHEMA.distribution'%r'" % SCHEMA.distribution)
-        log.info("Checking SCHEMA.Dataset'%r'" % SCHEMA.Dataset)
-        log.info("Checking SCHEMA.url '%r'" % SCHEMA.url)
-        log.info("Checking SCHEMA.uri '%r'" % SCHEMA.uri)
-        log.info("Checking dataset['uri'] '%r'" % dataset['uri'])
-        #eq_(unicode(dataset_ref), utils.dataset_uri(dataset))
-
-            # Basic fields
-
-        assert self._triple(g, dataset_ref, RDF.type, SCHEMA.Dataset)
-        #assert self._triple(g, dataset_ref, SCHEMA.url, dataset['uri'])
-        assert self._triple(g, dataset_ref, SCHEMA.name, dataset['title'])
-        assert self._triple(g, dataset_ref, SCHEMA.version, dataset['version'])
-        assert self._triple(g, dataset_ref, RDF.type, SCHEMA.distribution)
+        # Basic fields
+        assert self._triple(g, dataset_ref_changed, RDF.type, SCHEMA.Dataset)
+        assert self._triple(g, dataset_ref_changed, SCHEMA.name, dataset['title'])
+        assert self._triple(g, dataset_ref_changed, SCHEMA.version, dataset['version'])
