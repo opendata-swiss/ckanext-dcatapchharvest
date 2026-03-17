eeb99f0c393094cfb43d716056a537782051393c
Stefanie Taepke <stefina@users.noreply.github.com>
2026-03-17 14:16:20 +0100

    feat: implement IDCATURIGenerator

--- ckanext/dcatapchharvest/plugins.py
+++ ckanext/dcatapchharvest/plugins.py
@@ -1,12 +1,14 @@
 import os
 
+import ckan.plugins as plugins
+from ckanext.dcat.interfaces import IDCATURIGenerator
 from ckanext.dcat.plugins import DCATPlugin
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 
 class OgdchDcatPlugin(DCATPlugin):
-    pass
+    plugins.implements(IDCATURIGenerator, inherit=True)
 
     def after_show(self, context, data_dict):
         """
