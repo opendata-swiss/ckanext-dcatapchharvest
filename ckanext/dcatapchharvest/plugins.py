7db037a1ae7ffa2542d7c20d4eacad77bd28ec30
Stefanie Taepke <stefina@users.noreply.github.com>
2026-03-17 14:17:29 +0100

    fix: override dataset and resource uri with our helper functions
    The functions are already in place when the catalog.jsonld is generated but did not match the catalog-ids in the dataset.jsonld

--- ckanext/dcatapchharvest/plugins.py
+++ ckanext/dcatapchharvest/plugins.py
@@ -1,14 +1,34 @@
 import os
 
 import ckan.plugins as plugins
+from rdflib import URIRef
+
 from ckanext.dcat.interfaces import IDCATURIGenerator
 from ckanext.dcat.plugins import DCATPlugin
+
+from . import dcat_helpers
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 
 class OgdchDcatPlugin(DCATPlugin):
     plugins.implements(IDCATURIGenerator, inherit=True)
+
+    def dataset_uri(self, dataset_dict, default_uri):
+        """
+        Return the dataset URI for RDF serializations (make sure the URL matches
+        the environment and that we use the permalink).
+        """
+        dataset_ref = URIRef(default_uri) if default_uri else None
+        return dcat_helpers.dataset_uri(dataset_dict, dataset_ref)
+
+    def resource_uri(self, resource_dict, default_uri):
+        """
+        Return the resource URI for RDF serializations (make sure the URL matches
+        the environment).
+        """
+        distribution = URIRef(default_uri) if default_uri else None
+        return dcat_helpers.resource_uri(resource_dict, distribution)
 
     def after_show(self, context, data_dict):
         """
